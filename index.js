91a4a98375df5a27410d610049620c08bf60e451
khai96_ <hvksmr1996@gmail.com>
2018-01-20 01:18:29 +0700

    Move execution code out of index.js

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
 #! /usr/bin/env node
-'use strict'
-
-const path = require('path')
-const {env} = require('process')
-const {createServer} = require('http')
-const next = require('next')
-const app = next(path.resolve(__dirname, 'pages'))
-const handle = app.getRequestHandler()
-const port = Number(env.PORT || 5000)
-
-console.info('[INFO] Serving...', {port})
-createServer(handle).listen(port)
+require('./server.js')
