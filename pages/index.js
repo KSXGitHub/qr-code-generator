a7d406fe977de21785a86f19bd281a7a8a776db4
khai96_ <hvksmr1996@gmail.com>
2018-01-19 16:06:01 +0700

    Complete

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,23 @@
-import AnalogClock from 'react-analog-clock'
-export default () => <AnalogClock />
+import AnalogClock, {Themes} from 'react-analog-clock'
+import Remarkable from 'react-remarkable'
+
+export default () => <section>
+  <Remarkable>
+    Why create your own clock when you can just use others'
+    [prebuilt component](https://goo.gl/tb3fFb)?
+  </Remarkable>
+  <main>
+    <AnalogClock theme={Themes.dark} />
+  </main>
+  <footer>
+    <Remarkable>
+      Created by [Hoàng Văn Khải](https://github.com/KSXGitHub) &copy; 2017.
+    </Remarkable>
+    <Remarkable>
+      Powered by [AnalogClock](https://goo.gl/tb3fFb) and [Next.js](https://goo.gl/pTP4B1).
+    </Remarkable>
+    <Remarkable>
+      Source code is available at [GitHub/KSXGitHub/analog-clock](https://goo.gl/V6Bff4).
+    </Remarkable>
+  </footer>
+</section>
