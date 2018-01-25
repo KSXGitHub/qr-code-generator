61f4d724bfec99552ea4f310ad0cd21b511c9e86
khai96_ <hvksmr1996@gmail.com>
2018-01-25 22:05:47 +0700

    No needs to invoke another process

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
 #! /usr/bin/env node
-const {resolve} = require('path')
-const {spawn} = require('child_process')
-const {env, exit, stdout, stderr} = require('process')
-
-const child = spawn('node', [resolve(__dirname, 'server.js')], {
-  env: {
-    ...env,
-    NODE_ENV: 'production'
-  }
-})
-
-child.on('exit', (status, signal) => {
-  signal && console.info({status, signal})
-  exit(status)
-})
-
-child.stdout.on('data', chunk => stdout.write(chunk))
-child.stderr.on('data', chunk => stderr.write(chunk))
+require('process').env.NODE_ENV='production'
+require('./server.js')
